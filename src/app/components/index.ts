18888adfa90428a0c158edc0b61b75fbf1cd5453
Facundo <floresfacundo68@gmail.com>
2022-11-21 14:07:15 -0300

    add index.ts for components in app

---
+++ src/app/components/index.ts
@@ -0,0 +1,7 @@
+export { default as NewLineText } from './NewLineText';
+export { default as TaskItem } from './TaskItem';
+export { default as TaskSection } from './TaskSection';
+export { default as TestCard } from './TestCard';
+export { default as TestGrid } from './TestGrid';
+export { default as Transcription } from './Transcription';
+export { default as VideoPlayer } from './VideoPlayer';
