207eb4a25af1586b81ce0089c859f3a24932c8fc
Facundo <floresfacundo68@gmail.com>
2022-11-21 14:00:24 -0300

    update in api: new function to get test by id

--- src/api/api.ts
+++ src/api/api.ts
@@ -7,4 +7,14 @@ export const api = {
       return DATA;
     },
   },
+  test: {
+    // I choosed to use index as an id since the db.json has repeated ID values
+    getById: async (id: number): Promise<Test> => {
+      if (id > DATA.length) {
+        throw new Error('The id does not exist in the database');
+      }
+
+      return DATA[id - 1];
+    },
+  },
 };
